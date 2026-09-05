b6e357d817fbc7dbd904adc4aa1711cd13e330cd
HeitorDalla <heitorvillavilla@gmail.com>
2026-09-05 10:54:20 -0300

    feat(terraform): adicionar output da chave KMS

--- core/outputs.tf
+++ core/outputs.tf
@@ -1,9 +1,14 @@
 # VPC
 output "network_id" {
-  value = google_compute_network.vpc_global.id
+  value = google_compute_network.main.id
 }
 
 # Subnets
 output "subnet_id" {
-  value = google_compute_subnetwork.subnet_web.id
+  value = google_compute_subnetwork.app.id
+}
+
+# KMS
+output "kms_key" {
+  value = google_kms_crypto_key.symkey.id
 }
